1959453475f112d684bf7913a92dd1d0f6234180
SaadRegal <saadrg3@gmail.com>
2018-11-16 15:55:29 +0100

    Refactored data retrievement+User can save all his settings
    No more setIntervale

--- view-src/src/app/Components/home/home.component.ts
+++ view-src/src/app/Components/home/home.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {DragulaService} from 'ng2-dragula';
 import {RemoteService} from '../../Services/remote/remote.service';
-import {retry} from "rxjs/operators";
 
 declare let $: any;
 
@@ -34,7 +33,7 @@ export class HomeComponent implements OnInit {
         },
         layout: {
           count: 3,
-          order: ['saad_regal', 'newsycombinator', 'ycombinator'],
+          order: ['MakeSchool', 'newsycombinator', 'ycombinator'],
           timeRange: {
             max: new Date(Date.now()),
             min: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 5)
@@ -50,11 +49,6 @@ export class HomeComponent implements OnInit {
   acceptedColors = 'blue purple pink teal green grey orange black violet green red olive';
   tweets: any;
   filteredTweets: any;
-  waitForRemote: any;
-  waitForFilter: any;
-  retry: any;
-  retryFilter: any;
-  isDisplayed: boolean;
 
 
   ngOnInit() {
@@ -62,76 +56,48 @@ export class HomeComponent implements OnInit {
   }
 
   loadTweets() {
-    // this.tweets = [];
-    // this.filteredTweets=[];
+    this.tweets = [];
+    $('.noResult').remove();
+    $('.placeholderSegment').css('display', 'block');
+
     console.log('loading tweets');
     this.getTweets();
-    this.retry = 0;
-    this.waitForRemote = setInterval(() => {
-      console.log('working on remote')
-      if (this.tweets.length === this.settings.layout.order.length) {
-        console.log('tweets reloaded');
-        clearInterval(this.waitForRemote);
-        for (let i = 0; i < this.settings.layout.order.length; i++) {
-          for (const userTweets of this.tweets) {
-            if (userTweets[0]) {
-              if (userTweets[0].user.screen_name === this.settings.layout.order[i]) {
-                this.filteredTweets.push(userTweets);
-              }
-            }
-          }
-        }
-        this.retryFilter = 0;
-        this.waitForFilter = setInterval(() => {
-          if (this.filteredTweets.length === this.settings.layout.order.length) {
-            this.displayColumns();
-            clearInterval(this.waitForFilter);
-          } else {
-            this.retryFilter++;
-            if (this.retryFilter > 3) {
-              this.displayColumns();
-              clearInterval(this.waitForFilter);
-            }
-          }
-
-        }, 500);
-      } else {
-        this.retry++;
-        if (this.retry > 15) {
-          this.displayColumns();
-          clearInterval(this.waitForRemote);
-        }
-      }
-    }, 500);
-
-    setTimeout(() => {
-      clearInterval(this.waitForRemote);
-      clearInterval(this.waitForFilter);
-      this.displayColumns();
-    }, 1000 * 20);
   }
 
   displayColumns() {
-    if (!this.isDisplayed) {
-      this.isDisplayed = true;
-      this.loadSettings();
-      this.initUI();
-      $('.placeholderColumn').remove();
-      $('.feedColumn').css('display', 'block');
-    }
+
+    this.loadSettings();
+    this.initUI();
+    $('.placeholderSegment').css('display', 'none');
+    $('.feedColumn').css('display', 'block');
+
   }
 
-  getTweets() {
-    for (const user of this.settings.layout.order) {
-      this.remote.getTweets(user, this.settings.layout.count).subscribe((data: any) => {
-        let newArr = data;
-        newArr = newArr.filter((item: any) => {
-          return new Date(item.created_at) >= new Date(this.settings.layout.timeRange.min) &&
-            new Date(item.created_at) <= new Date(this.settings.layout.timeRange.max);
+  async getTweets() {
+    const users = this.settings.layout.order;
+    for (let i = 0; i < users.length; i++) {
+      console.log(i);
+      await new Promise(resolve => {
+        this.remote.getTweets(users[i], this.settings.layout.count).subscribe((data: any) => {
+          let newArr = data;
+          newArr = newArr.filter((item: any) => {
+            return new Date(item.created_at) >= new Date(this.settings.layout.timeRange.min) &&
+              new Date(item.created_at) <= new Date(this.settings.layout.timeRange.max);
+          });
+          console.log(newArr);
+          this.tweets.push(newArr);
+          if (data) {
+            resolve();
+          }
+          if (i === users.length - 1) {
+            setTimeout(() => {
+              this.displayColumns();
+            }, 500);
+          }
         });
-        console.log(newArr);
-        this.tweets.push(newArr);
+
       });
+
     }
   }
 
@@ -169,7 +135,7 @@ export class HomeComponent implements OnInit {
       $(feed).find('.ui.header').css('color', '#fff').removeClass(this.acceptedColors);
       $(feed).find('.summary>.date').css('color', 'rgba(255,255,255,0.6)');
       $(feed).find('.summary').css('color', 'rgba(255,255,255,0.6)');
-      $(feed).find('.summary>a').css('color', 'rgba(255,255,255,0.6)');
+      $(feed).find('.summary>span>a').css('color', 'rgba(255,255,255,1)');
       $(feed).find('.extra.text').css('color', '#fff');
       $(feed).find('.meta>span').css('color', 'rgba(255,255,255,0.65)');
       $(feed).find('.meta>span>a').css('color', 'rgba(255,255,255,0.4)');
@@ -179,7 +145,7 @@ export class HomeComponent implements OnInit {
       $(feed).find('.ui.header').addClass(this.settings.theme.color);
       $(feed).find('.summary>.date').css('color', 'rgba(0,0,0,.4)');
       $(feed).find('.summary').css('color', 'rgba(0,0,0,.4)');
-      $(feed).find('.summary>a').css('color', '#4183c4');
+      $(feed).find('.summary>span>a').css('color', '#4183c4');
       $(feed).find('.extra.text').css('color', '#000');
       $(feed).find('.meta>span').css('color', 'rgba(0,0,0,.6)');
       $(feed).find('.meta>span>a').css('color', 'rgba(0,0,0,.6)');
@@ -198,16 +164,19 @@ export class HomeComponent implements OnInit {
       onChange: (value) => {
         this.settings.layout.timeRange.min = value;
         setTimeout(() => {
+          this.saveSettings();
           this.loadTweets();
-        }, 1500);
+        }, 2000);
       }
     });
     $('.timeRange.max').calendar({
       onChange: (value) => {
         this.settings.layout.timeRange.max = value;
         setTimeout(() => {
+          console.log('calendar change')
+          this.saveSettings();
           this.loadTweets();
-        }, 1500);
+        }, 2000);
 
       }
     });
@@ -215,6 +184,12 @@ export class HomeComponent implements OnInit {
       onChange: () => {
         this.saveSettings();
       }
+    });
+    $('#count').on('change', () => {
+      setTimeout(() => {
+        this.saveSettings();
+        this.loadTweets();
+      }, 1000);
     });
   }
 
--- view-src/src/app/Services/remote/remote.service.ts
+++ view-src/src/app/Services/remote/remote.service.ts
@@ -1,17 +1,23 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RemoteService {
   host: String = 'localhost';
-  apiUrl: String = 'http://' + this.host + ':7890/1.1/statuses/user_timeline.json';
+  readonly apiUrl: any = 'http://' + this.host + ':7890/1.1/statuses/user_timeline.json';
 
   constructor(private http: HttpClient) {
   }
+
   getTweets(user, count) {
     // console.log('d');
-    return this.http.get(this.apiUrl + '?count=' + count + '&screen_name=' + user );
+
+    const params = new HttpParams()
+      .set('count', count)
+      .set('screen_name', user);
+    return this.http.get(this.apiUrl, {params});
   }
 }
